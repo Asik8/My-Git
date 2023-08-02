1de57fe88255dd7cf804f082e3277e87053e2b13
Asik8 <motasembillah84@gmail.com>
2023-08-02 18:51:34 +0600

    JavaScript Module 18 finished

---
+++ Java-Script/Module 18/PracticeProblem.js
@@ -0,0 +1,110 @@
+// Problem 1
+
+// for (var i=0;i<39;i++)
+// {
+//     console.log('Ajke amar mon valo nei');
+// }
+
+// var i=0;
+// while(i<39)
+// {
+//     console.log('Ajke amar mon valo nei');
+//     i++;
+// }
+
+
+
+// Problem 2
+
+// for (var i=58;i<99;i++)
+//  {
+//     console.log(i);
+//  }
+
+// var i=58;
+// while(i<99)
+// {
+//      console.log(i);
+//    i++;
+// }
+
+
+// Problem 3
+
+// for (var i=412;i<457;i+=2)
+//  {
+//     console.log(i);
+//  }
+
+// var i=411;
+// while(i<457)
+// {
+//      console.log(i);
+//    i+=2;
+// }
+
+
+// Problem 4
+
+// for (var i=581;i<624;i+=2)
+//  {
+//     console.log(i);
+//  }
+
+//  var i=581;
+//  while(i<624)
+//  {
+//     console.log(i);
+//     i+=2;
+// }
+
+// Problem 6
+
+// var topics=["HTML", "CSS", "Tailwind", "Daisy UI", "Bootstrap", "JavaScript"];
+
+// for (var i=0; i<topics.length;i++)
+// {
+//     var output=topics[i];
+//     console.log(output);
+// }
+
+// Problem 7
+
+// var topics=["Nokia", "Walton", "Samsung", "Huwei", "Motorola", "Realme", "Vivo", "Tecno"];
+// var i=0;
+
+// while ( i<topics.length)
+// {
+//     var output=topics[i];
+//     console.log(output);
+//     i++;
+// }
+
+
+// Problem 8
+
+// var i=38;
+
+// while ( i<87)
+// {
+//     if(i>44)
+//     {
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+
+// Problem 9
+var i=38;
+
+while ( i<87)
+{
+    if(i>44)
+    {
+        continue;
+    }
+    console.log(i);
+    i++;
+}
