92daef46035124925585f7033cc73916b1eec4c2
Asik8 <motasembillah84@gmail.com>
2023-08-03 11:24:48 +0600

    Final

---
+++ Java-Script/Module 19/PracticeProblem.js
@@ -0,0 +1,25 @@
+// Problem 1
+// function foo(){
+//     // console.log(foo);
+// }
+
+// function bar(){
+//     console.log(bar);
+// }
+
+// foo(console.log(foo));
+// // Boo();
+
+// Problem 2
+// function avg(n1,n2,n3){
+//     var sum=n1+n2+n3;
+//     var av=sum/3;
+//     console.log(av);
+// }
+
+// var n1=10;
+// var n2=10;
+// var n3=1;
+
+// avg(n1,n2,n3);
+
